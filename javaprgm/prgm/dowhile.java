38268a330779b67fe5040c8c6337f9a8c86c272c
sahana591 <nsahana591@gmail.com>
2021-11-10 21:27:38 +0530

    okey

---
+++ javaprgm/prgm/dowhile.java
@@ -0,0 +1,9 @@
+class Test{
+    public static void main(String[] args){
+       int i=10;
+       do{
+            System.out.println("HEllo,GA");
+            i++;
+       }while(i<=14);
+    }
+}
